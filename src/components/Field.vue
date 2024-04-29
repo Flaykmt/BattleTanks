bca28024487c36bba1c28fff28272955d2347214
Flayka <the.flayka@gmail.com>
2024-04-29 18:25:00 +0500

    Add New Field

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -1,30 +1,46 @@
 <template>
 	<div class="field">
-		<div class="field-player" v-for="player in 2">
-			<h2>{{ player === 1 ? 'First' : 'Second' }} Player:</h2>
+		<div class="field-player">
+			<h2>First Player:</h2>
 			<div class="field-player-ground">
-				<div class="field_cell" v-for="item in 100"></div>
+				<div class="field_cell" v-for="(item, idx) in cell" :key="idx">
+					<div class="field_cell" v-for="(cell, index) in item" :key="index"></div>
+				</div>
 			</div>
 		</div>
 	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+interface ICell {
+	active: boolean
+}
+
+const cell: Array<Array<ICell>> = reactive([])
+
+for (let i = 0; i < 10; i++) {
+	cell.push([])
+	for (let idx = 0; idx < 10; idx++) {
+		cell[i].push({ active: false })
+	}
+}
+
+console.log(cell);
+
+</script>
 
 <style scoped lang="scss">
 .field {
-  display: flex;
-  justify-content: space-between;
+	display: flex;
+	justify-content: space-between;
 	&-player {
 		h2 {
 			font-size: 40px;
 		}
 		&-ground {
-			display: grid;
-			width: max-content;
-			grid-template-columns: repeat(10, 1fr);
-			grid-template-rows: repeat(10, 1fr);
-			margin: 20px 0;
+			display: flex;
 		}
 	}
 }
@@ -33,5 +49,9 @@
 	border: 1px solid black;
 	width: 50px;
 	height: 50px;
+}
+
+.active {
+	background-color: black;
 }
 </style>
